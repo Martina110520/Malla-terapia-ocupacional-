48284a71eacf20f272132dfff04852360a37a177
Martina110520 <hernandezysmarle09@gmail.com>
2025-08-29 16:55:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const botones = document.querySelectorAll(".toggle");
 
+  botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+      const contenido = boton.nextElementSibling;
+
+      if (contenido.style.maxHeight) {
+        contenido.style.maxHeight = null;
+      } else {
+        contenido.style.maxHeight = contenido.scrollHeight + "px";
+      }
+    });
+  });
+});
